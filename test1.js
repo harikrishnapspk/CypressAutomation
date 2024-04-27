d54ba1581e8b7c787a1ab5509e66c44426e1208b
HariKrishna <harikrishnapspk7@gmail.com>
2024-04-27 11:09:16 +0530

    java script latest code

---
+++ test1.js
@@ -0,0 +1,50 @@
+const prompt = require('prompt-sync')();
+/*// the hello world program
+console.log("Hello World");
+
+//Add Two Numbers
+let a=2;
+let b=3;
+let c= a+b;
+console.log(`Sum of ${a} and ${b} is : ${c} `);
+
+
+
+//JavaScript Program to Find the Square Root
+let a=prompt("enter the givin number : ")
+let result=Math.sqrt(a);
+console.log(`The Square Of ${a} is square of ${result}`);
+
+
+
+//JavaScript Program to Calculate the Area of a Triangle
+//Area When Base and Height is Known
+let Height=prompt("Enter the heght: ")
+let base = prompt("Enter The Base Value : ")
+let Area = (Height*base)/2
+console.log(`The Area of Triangle is : ${Ar}`)
+
+//JavaScript Program to Swap Two Variables
+let a = prompt("enter the firstr number : ")
+let b= prompt("Enter the second number : ")
+let temp;
+temp = a;
+a=b;
+b=temp;
+console.log(`the value of a after swapping : ${a}`)
+console.log(`the value of b after swapping : ${b}`)
+
+//JavaScript program to swap two variables using destructuring assignment
+const a = prompt("enter the firstr number : ")
+const b= prompt("Enter the second number : ")                                       //doubt
+[a,b] = [b,a]
+console.log(`the value of a after swapping : ${a}`)
+console.log(`the value of b after swapping : ${b}`)
+
+//Generate a Random Number
+
+const a = Math.random()
+console.log(a)
+//Get a Random Number between 1 and 10
+const b = Math.random()*(10-1)+1;
+console.log(`random value between 1 and 10 is ${b}`);*/
