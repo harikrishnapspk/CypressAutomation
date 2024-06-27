e405eeded0b901a8937ca72fe68541a0634162cb
HariKrishna <harikrishnapspk7@gmail.com>
2024-06-27 13:14:41 +0530

    udpated framework files

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    "baseurl":"https://opensource-demo.orangehrmlive.com",
-    experimentalStudio:true,
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,16 +1,13 @@
-
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    projectId: "twcy9h",
     "baseurl":"https://opensource-demo.orangehrmlive.com",
     experimentalStudio:true,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
-   },
-  },
+    },
+  },
 });
-
