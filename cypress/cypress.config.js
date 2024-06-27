dfaf4f5ce6f6864f0c3d48ace69c7fda4fd7a79e
HariKrishna <harikrishnapspk7@gmail.com>
2024-06-27 10:55:18 +0530

    new

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    projectId: "twcy9h",
+    "baseurl":"https://opensource-demo.orangehrmlive.com",
+    experimentalStudio:true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
-    },
-  },
+   },
+  },
 });
+
